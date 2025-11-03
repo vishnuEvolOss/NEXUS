2ea6a5dc5320708bcf94543c7fc2d41ea75aa7ea
vishnuEvolOss <vishnuvettiyil11@gmail.com>
2025-11-03 21:50:13 +0530

    dist folder served to backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,13 +21,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
 // make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../frontend/dist"))); // or ../frontend/build
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
